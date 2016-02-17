89f7495c903a728e4eab08b22e693e6282140168
IamElaya <asison13@gmail.com>
2016-02-17 04:31:10 +0000

    Web scraper works

--- index.js
+++ index.js
@@ -6,39 +6,59 @@ var app     = express();
 
 
 app.get('/scrape', function(req, res){
-  site = 'http://substack.net/images/'
+  site = 'http://substack.net/images/';
   request(site, function (error, response, html) {
     if(!error){
       var $ = cheerio.load(html);
       var permission, url, type;
-      // var scraped = { permission : "", url : "", type : ""};
+      var scraped = [];
 
-      var scraped = $('tr').filter(function(){
+     $('tr').filter(function(){
         var data = $(this);
         permission = data.children().first().children().text();
         url = ("substack.net" + data.children().last().children().attr("href"));
         type = (data.children().last().children().attr("href")).split('.').pop();
 
-      //  return ({
-      //   permission : permission, 
-      //   url : url,
-      //   type : type
-      // });
-        // return scraped.permission = permission;
-        // return scraped.url = url;
-        // return scraped.type = type;
+       scraped.push({
+        permission : permission, 
+        url : url,
+        type : type
+      });
+       // console.log(scraped);
+       //  scraped.permission = permission;
+       //  scraped.url = url;
+       //  scraped.type = type;
         // console.log(json)
 
       });
+     console.log(scraped);
+    }
+
+    scraped.forEach(function (output){
       
-    } 
-      fs.writeFile('output.csv', scraped, 'utf8', function(err){
-      if (err) {
+      // object to string
+      var keys = Object.keys(output);
+      var values = keys.map(function(key) {
+        return output[key];
+      });
+
+      // var line = values.toString() + '\n';
+      var line = values.join(', ') + '\n';
+
+      fs.appendFile('output.csv', line, 'utf8', function(err){
+        if (err) {
         console.log(error);
       } else {
         console.log('File successfully written! - Check your project directory for the output.csv file');
       }
-    })
+      });
+    });
+
+
+     
+      // fs.writeFile('output.csv', scraped.toString().join('\n'), 'utf8', function(err){
+      
+
     res.send('Check your console!')
   
 
